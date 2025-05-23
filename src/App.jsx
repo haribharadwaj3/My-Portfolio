70a91e1d0329127e6ad9322f95f99c2aa35aab6e
Hari Bharadwaj <haribharadwaj6@gmail.com>
2025-05-23 12:57:22 +0530

    Analytics added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,30 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes, useLocation } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { NotFound } from "./pages/NotFound";
 import { Toaster } from "@/components/ui/toaster";
+import ReactGA from 'react-ga4';
+import { useEffect } from "react";
+
+// Initialize GA once
+ReactGA.initialize('G-X5N5NVECDE');
+
+// Track page views on route change
+function GAListener() {
+  const location = useLocation();
+
+  useEffect(() => {
+    ReactGA.send({ hitType: "pageview", page: location.pathname });
+  }, [location]);
+
+  return null;
+}
+
 function App() {
   return (
     <>
       <Toaster />
       <BrowserRouter>
+        <GAListener />
         <Routes>
           <Route index element={<Home />} />
           <Route path="*" element={<NotFound />} />
